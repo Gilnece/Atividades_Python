0adad20a39bd328284f4f45e169313df77568d7d
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-17 21:16:29 -0300

    correções visuais

--- Des027.py
+++ Des027.py
@@ -4,15 +4,15 @@ A multa vai custar R$7,99 oir casa KM acima do limite.'''
 
 from random import randint
 
-velocidade = randint(1, 300)
-multa = (velocidade - 80) * 7.99 
+print('Velocidade permitida: 80KM/h')
+velocidade = randint(1, 300) #subtendendo que a velocidade seja registrada em uma lombada eletronica
+multa = (velocidade - 80) * 7.99
 
 if velocidade > 80:
     print('Você está sendo multado por conduzir acima da velocidade permitida')
     print('Velocidade maxima permitida: 80KM/h')
     print('Velocidade registrada: {}'.format(velocidade))
-    print('Valor da multa: {}'.format(multa))
+    print('Valor da multa: {:.2f}R$'.format(multa))
 else:
-    print('Velocidade permitida: 80KM/h')
     print('Velocidade registrada: {}'.format(velocidade))
     print('Otima condução, cuidado na estrada!')
