e22468c1f92490c36ef4e77f5e94d14e6fe0499b
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-17 21:05:41 -0300

    import time
    adicionada uma biblioteca para adicionar uma pausa.
    também feita uma pequena melhoria no codigo para ficar visualmente mais bonito ao executar

--- Des026.py
+++ Des026.py
@@ -3,10 +3,16 @@ entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhid
 O programa deverá escrever na tela se o usuário venceu ou perdeu'''
 
 import random
+from time import sleep
+print('-=-'*23)
+print('Vou pensar em um numero entre 0 e 5. Tente adivinhar o computador...')
+print('-=-'*23)
 computador = random.randint(0,5)
-escolha = int(input('Digite um numero entre 0 e 5: '))
-
+escolha = int(input('tente adivinhar: '))
+print('PROCESSANDO...')
+sleep(2)
 if escolha == computador:
     print('Parabéns você acertou!')
 else:
-    print('Tente novamente na proxima')
+    print('Hahaha eu ganhei')
+    print('pensei no numero {}, mais sorte na proxima'.format(computador))
