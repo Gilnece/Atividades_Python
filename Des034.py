35c6a01de9d62d25be1083b7e1974e4d53079fb5
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-20 12:45:39 -0300

    analise de credito
    programa que analisa o credito, em consideração a prestação na compra de uma casa.
    inicialmente, o codigo não levava em consideração se vai ter entrada ou não!

---
+++ Des034.py
@@ -0,0 +1,31 @@
+'''Escreva um programa que aprove um emprestimo bancario
+para a compra de uma casa. O programa vai perguntar o VALOR DA CASA, o SALARIO
+do comprador e em QUANTOS ANOS ele vai pagar.
+Calcule o calor da prestação mensal, sabendo que ela não pode exceder a 30%
+do salario ou então o emprésto será negado'''
+
+
+print('Bem vindo a COMPRECASA, vamos consultar seu credito?')
+
+vcasa = float(input('Digite o valor da casa: '))
+salario = float(input('Quanto recebe por mes: '))
+qt_anos = int(input('Em quantos anos pretende pagar: '))
+
+tera = input('terá entrada?')
+mensalidade = qt_anos * 12
+
+if entrada == 'sim' or tera == 'sim':
+    vlentrada = float(input('Digite o valor de entrada: '))
+    prestacao = (vcasa - vlentrada) / mensalidade
+else:
+    prestacao = vcasa / mensalidade
+
+analise = salario * 0.3
+
+if prestacao > analise:
+    print('Infelizmente seu credito não foi aprovador')
+    print('o valor da prestação ficaria em R${:.2f}, 30% do seu salario é: R${:.2f}'.format(prestacao, analise))
+    print('\033[1;31mEmpréstimo Negado\033[0m')
+elif prestacao < analise:
+    print('\033[1;32mCredito aprovado com sucesso\033[0m')
+    print('o valor da prestação ficaria em R${:.2f}, 30% do seu salario é: R${:.2f}'.format(prestacao, analise))
