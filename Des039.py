c52e93f2a61b9c0164d7be19fd24eb815e43dc3c
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-21 12:21:59 -0300

    diminuindo o codigo
    apenas mudanças em pontos redundantes

--- Des039.py
+++ Des039.py
@@ -10,25 +10,21 @@ import time
 from datetime import date
 from time import sleep
 
-dia = date.today().day
-mes = date.today().month
 ano = date.today().year
 
 print ('Levando em consideração a data: {}/{}/{}'.format(dia, mes, ano))
 ano_nasc = int(input('Digite o ano de nascimento: '))
-
 ano_atleta = ano - ano_nasc
-
 print('\033[1;31mPROCESSANDO...\033[0m')
 sleep(1)
 
-if ano_atleta < 9:
+if ano_atleta <= 9:
     print('Com {} anos, é um atleta Mirim'.format(ano_atleta))
-elif 9 < ano_atleta < 14:
+elif ano_atleta <= 14:
     print('Com {} anos, é um atleta Infantil'.format(ano_atleta))
-elif 14 < ano_atleta < 19:
+elif ano_atleta <= 19:
     print('Com {}, é um atleta Junior'.format(ano_atleta))
-elif 19 < ano_atleta < 20:
+elif ano_atleta <= 25:
     print('Com {}, é um atleta Senior'.format(ano_atleta))
-elif 20 < ano_atleta:
+else:
     print('com {} anos, é um atleta Master'.format(ano_atleta))
