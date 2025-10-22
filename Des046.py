a14e373242f06f2ff9e45809fd5213da218236c9
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 19:32:44 -0300

    correção de codigo
    adicionado um contador de numeros solicitados
    adicionado a soma antes solicitada pela questão que tinha deixado passar

--- Des046.py
+++ Des046.py
@@ -1,6 +1,9 @@
-'''faça um programa que calcule a soma entre todo os números impares que são
-múltiplos de três e que se encontram no intervalo de 1 até 500'''
-for c in range(0, 500):
+soma = 0
+cont = 0
+for c in range(0, 500+1):
     if c % 2 == 1: # numeros impares
         if c % 3 == 0: # numeros multiplos de 3
-            print(c)
+            soma = soma + c # realizar a soma de todos os numeros informados
+            cont = cont + 1 # realiza a contagem de quantas interações foram feitas
+print('A soma entre todos os numeros solicitados é {}'.format(soma))
+print('{} numeros foram encontrados'.format(cont))
