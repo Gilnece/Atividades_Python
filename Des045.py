eda9ba40f3e18a010502e64528dff8595df81dce
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 19:21:36 -0300

    melhoria de codigo
    exibição melhor

--- Des045.py
+++ Des045.py
@@ -2,7 +2,8 @@
 no intervalo entre 1 e 50'''
 
 print('\nEssa é a lista de todos os numeros pares entre 1 e 50:')
-for c in range(0, 50):
+for c in range(1, 50+1): # para 2, 51, 2 é feito metade das interações
     par = c % 2
-    if par == 0:
-        print(c)
+    if par == 0: # para o metodo anterior falado não precisaria desse if
+        print(c, end=' ') # end faz não pular a linhas
+print('\nEsses são todos os numeros pares encontrados nessa condição')
