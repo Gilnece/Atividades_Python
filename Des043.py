70b82111857475df4c83bfa9e04ba3ff15061aa1
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 20:40:26 -0300

    correção de codigo
    resultados invertidos corrigido
    adicionado opção invalida ao escolher algo diferente de pedra, papel ou tesoura

--- Des043.py
+++ Des043.py
@@ -1,5 +1,5 @@
 '''crie um programa que faça o computador jogar jokenpô com você'''
-from random import choice
+import random
 from time import sleep
 
 print('-=-' * 23)
@@ -8,29 +8,30 @@ print('-=-' * 23)
 
 opcoes = ['pedra', 'papel', 'tesoura']
 
-computador = choice(opcoes)
-escolha = input('Vamos,tente acertar e ganhar de mim!\n ')
+computador = random.choice(opcoes)
+escolha = input('Vamos,tente acertar e ganhar de mim: ')
 print('PROCESSANDO...')
-sleep(2)
+sleep(1)
 if computador == escolha:
     print('Vamos novamente! Escolhemos {}'.format(computador))
 elif computador == 'pedra' and escolha == 'tesoura':
-    print('Mais sorte na proxima, eu ganhei')
-    print('{} perde de {}'.format(escolha, computador))
+    print('mais sorte na proxima')
+    print('{} ganha de {}'.format(computador, escolha))
 elif computador == 'papel' and escolha == 'pedra':
-    print('Mais sorte na proxima, eu ganhei')
-    print('{} perde de {}'.format(escolha, computador))
+    print('mais sorte na proxima')
+    print('{} ganha de {}'.format(computador, escolha))
 elif computador == 'tesoura' and escolha == 'papel':
-    print('Mais sorte na proxima, eu ganhei')
-    print('{} perde de {}'.format(escolha, computador))
+    print('mais sorte na proxima')
+    print('{} ganha de {}'.format(computador, escolha))
 
 elif computador == 'tesoura' and escolha == 'pedra':
-    print('Muito bem, Você ganhou, escolhi {}'.format(computador))
-    print('{} ganha de {}'.format(escolha, computador))
+    print('muito bem você ganhou, escolhi {}'.format(computador))
+    print('{} perde de {}'.format(computador, escolha))
 elif computador == 'pedra' and escolha == 'papel':
-    print('Muito bem, Você ganhou, escolhi {}'.format(computador))
-    print('{} ganha de {}'.format(escolha, computador))
+    print('muito bem você ganhou, escolhi {}'.format(computador))
+    print('{} perde de {}'.format(computador, escolha))
 elif computador == 'papel' and escolha == 'tesoura':
-    print('Muito bem, Você ganhou, escolhi {}'.format(computador))
-    print('{} ganha de {}'.format(escolha, computador))
-
+    print('muito bem você ganhou, escolhi {}'.format(computador))
+    print('{} perde de {}'.format(computador, escolha))
+elif escolha != 'pedra' and escolha != 'tesoura' and escolha != 'papel':
+    print('Deixa de ser burro e escolhe certo!')
