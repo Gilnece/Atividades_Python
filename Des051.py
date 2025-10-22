6c7c96389abb58ef3e9985db6a4e9e423808c9a2
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 14:51:14 -0300

    analise de palindromo
    programa realiza a leitura de uma palavra ou frase e diz se ela é ou não um palíndromo

---
+++ Des051.py
@@ -0,0 +1,16 @@
+'''Crie um programa que leia uma frase qualquer e diga se ela é um
+palindromo, desconsiderando os espaços'''
+#palindromo são palavras ou frases que podem ser lidas iguais de trás para frente
+#apos a sopa
+#a sacada da casa
+#a torre da derrota
+#o lobo ama o bolo
+#anotaram a data da maratona
+
+texto = str(input('Digite uma frase qualquer: '))
+frase = texto # armazenar o testo para exibir no final
+texto = texto.replace(' ', '').lower() # tratando o texto para tirar os espaços e deixar menusculo
+if texto == texto[::-1]: # comparando frase inicial com o oposto dela
+    print('A frase {}, é um palindromo'.format(frase.upper()))
+else:
+    print('A frase {} não forma um palindromo'.format(frase.upper()))
