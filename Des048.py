7f159d6fa7811f6918be54544c6e8ba20fe3f762
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 19:52:21 -0300

    adicionado contador
    além de adicionar um contado, foi também formatado a exibição da numeração de interações

--- Des048.py
+++ Des048.py
@@ -2,10 +2,12 @@
 daqueles que forem pares. Se o valor digitado for impar desconsidere-o'''
 
 soma = 0 #iniciar a soma sem valor nenhum para trabalhar ele posteriormente
-for c in range(0, 6):
-    n = int(input('Digite um numero inteiro: '))
+cont = 0
+for c in range(1, 6+1):
+    n = int(input('Digite o {}º numero inteiro: '.format(c)))
     if n % 2 == 0: # irá destacar apenas o numeros pares
         soma += n # soma os valores listados acima
-print ('A soma dos numero pares é: {}'.format(soma))
+        cont += 1
+print('{} numeros pares foram encontrados e a soma dos numero pares é: {}'.format(cont, soma))
 if n % 2 == 1:
     print('Nenhum numero PAR encontrado!')
