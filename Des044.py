aab6a74de4b1f653f86caf5375d6c80684031f91
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 14:06:47 -0300

    contagem regressiva
    criado um programa capaz de mostra em tela uma contagem regressiva de 10 a 0, com intervalo de 1 segundo entre casa numero

---
+++ Des044.py
@@ -0,0 +1,9 @@
+'''faça um programa que mostre na tela uma contagem regressiva de artificio,
+indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
+
+from time import sleep
+
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print('Feliz ano novo!!!')
