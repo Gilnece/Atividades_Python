29bdd0d04443c024fdd140a73cb14dff44c3f133
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-22 20:01:53 -0300

    Update Des049.py

--- Des049.py
+++ Des049.py
@@ -6,10 +6,12 @@ a1 = int(input('Digite o primeiro digito do termo: '))
 r = int(input('Digite a Razão da PA: '))
 an = int(input('Informe o ultimo digito do termo: '))
 
-contador = 0  # vai contar quantos termos foram exibidos
-
-for termo in range(a1, an + 1, r):
-    print(termo)
-    contador += 1
-    if contador == 10:  # para quando chegar a 10 termos, pare de exibir
+cont = 0  # iniciando contador
+print('-=-'*10)
+print('10 PRIMEIROS TERMOS DE UMA PA')
+print('-=-'*10)
+for pa in range(a1, an + 1, r):
+    print(pa, end=' ')
+    cont += 1
+    if cont == 10:  # para quando chegar a 10 termos, pare de exibir
         break
