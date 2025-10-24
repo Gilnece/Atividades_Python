f25b5601c8b5fe982d30549bbccbf19eab64d55f
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-24 11:17:21 -0300

    melhorias do codigo
    adicionado método para informar quantas pessoas serão analisadas

--- Des054.py
+++ Des054.py
@@ -4,32 +4,42 @@ A média de idade do grupo
 qual é o nome do homem mais velho
 quantas mulheres têm menos de 20 anos'''
 
-# Inicializando variáveis
-s_idade = 0 # soma das idades
-hmv = '' # homem mais velho
-ihmv = 0 # idade do homem mais velho
-mu_men_20 = 0 # mulheres menores que 20 anos
+p = int(input('Quantas pessoas iram ser analisadas? '))
 
-for c in range(1, 5): # Loop para cadastrar 4 pessoas
-    print('----- {}ª Pessoa -----'.format(c))
-    nome = input('Nome: ')
-    idade = int(input('Idade: '))
-    sexo = input('Sexo (M/F): ').strip().upper()  # padroniza entrada
-    # Soma das idades para calcular a média
-    s_idade += idade
+if p < 0:
+    print('Nenhuma pessoa para analisar!')
+else:
+    # Inicializando variáveis
+    soma_idade = 0
+    homem_mais_velho = ''
+    idade_homem_mais_velho = 0
+    mulheres_menores_20 = 0
 
-    if sexo == 'M': # Verifica se é homem e se é o mais velho
-        if idade > ihmv:
-            ihmv = idade
-            hmv = nome
+    # Loop para analisar a quantidade de pessoas informada anteriormente
+    for c in range(1, p+1):
+        print('----- {}ª Pessoa -----'.format(c))
+        nome = input('Nome: ')
+        idade = int(input('Idade: '))
+        sexo = input('Sexo (M/F): ').strip().upper()  # padroniza entrada
 
-    if sexo == 'F' and idade < 20: # Conta mulheres com menos de 20 anos
-        mu_men_20 += 1
+        # Soma das idades para calcular a média
+        soma_idade += idade
 
-media_idade = s_idade / 4 # Cálculo da média de idade
+        # Verifica se é homem e se é o mais velho
+        if sexo == 'M':
+            if idade > idade_homem_mais_velho:
+                idade_homem_mais_velho = idade
+                homem_mais_velho = nome
 
-# Resultado final
-print('----- RESULTADOS -----')
-print('A média de idade do grupo é {:.1f} anos'.format(media_idade))
-print('O homem mais velho é: {}'.format(hmv.capitalize()))
-print('Número de mulheres com menos de 20 anos: {}'.format(mu_men_20))
+        # Conta mulheres com menos de 20 anos
+        if sexo == 'F' and idade < 20:
+            mulheres_menores_20 += 1
+    # Cálculo da média de idade
+    media_idade = soma_idade / p
+
+    # Resultado final
+    print('----- RESULTADOS -----')
+    print('A média de idade do grupo é {:.2f} anos.'.format(media_idade))
+    print('O homem mais velho é: {}'.format(homem_mais_velho))
+    print('Número de mulheres com menos de 20 anos: {}'.format(mulheres_menores_20))
+
