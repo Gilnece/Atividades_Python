26f322bf09d414b0066c85966f00ca2449daac6b
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-24 09:47:34 -0300

    validação e correção
    realizada validação para quando o usuario informar um total de zero pessoas para analise, e também removidas as variáveis menor e maior de dentro do primeiro laço for, assim evitando a interação N vezes, recebendo o resultado somente ao fim do laço

--- Des053.py
+++ Des053.py
@@ -2,17 +2,20 @@
 mostre qual foi o maior e o menor peso lidos'''
 
 p = int(input('Quantas pessoas gostaria de analisar? '))
-# inicializando
-pesos = [] # cria uma lista para receber os pesos que seram informados posteriormente
-for c in range(1, p+1): # for para informar os pesos
-    peso = float(input('Digite o peso: '))
-    pesos.append(peso) # anexa os pesos a lista
-    maior = pesos[0] # atribui o peso maior a variavel maior
-    menor = pesos[0] # atribui o peso menor a variavel menor
-for peso in pesos: # compara os pesos informados, a lista que recebeu os pesos
-    if peso > maior: # se o peso informado for o maior contido dentro da lista
-        maior = peso # a variavel maior, irá receber o maior valor da lista
-    elif peso < menor: # se o peso informado for o menor contido dentro da lista
-        menor = peso # a variavel menor, irá receber o menor valor da lista
-print('O menor peso é {}Kg'.format(menor))
-print('O maior peso é {}Kg'.format(maior))
+if p <= 0:
+    print('Nenhuma pessoa para analisar!') # adicionada validação para quando o usuario informar um total de zero pessoas para serem analisadas
+else:
+    # inicializando
+    pesos = [] # cria uma lista para receber os pesos que seram informados posteriormente
+    for c in range(p): # for para informar os pesos
+        peso = float(input('Digite o peso: '))
+        pesos.append(peso) # anexa os pesos a lista
+    maior = pesos[0] # atribui o peso maior a variavel maior  / removido de dentro do laço for para que não se repita a interação diversas vezes
+    menor = pesos[0] # atribui o peso menor a variavel menor  / removido de dentro do laço for para que não se repita a interação diversas vezes
+    for peso in pesos: # compara os pesos informados, a lista que recebeu os pesos
+        if peso > maior: # se o peso informado for o maior contido dentro da lista
+            maior = peso # a variavel maior, irá receber o maior valor da lista
+        elif peso < menor: # se o peso informado for o menor contido dentro da lista
+            menor = peso # a variavel menor, irá receber o menor valor da lista
+    print('O menor peso é {}Kg'.format(menor))
+    print('O maior peso é {}Kg'.format(maior))
