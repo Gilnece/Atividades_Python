4ac247f16d754debf2da99296b8f3f776c576e81
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-24 09:24:53 -0300

    adicionado comentarios
    comentários referente ao funcionamento dos métodos

--- Des052.py
+++ Des052.py
@@ -6,9 +6,9 @@ from datetime import date
 pessoas = int(input('Quantos seram analisados? ')) # criado metodo posteriormente para definir quantas pessoas teram sua idade analisada
 menor = 0 # iniciar contagem em zero
 maior = 0 # iniciar contagem em zero
-for i in range(1, pessoas+1): # criar um range que inicia em 1 e vai até 6, que também pode ser escrito com 6+1
-    idade = int(input('Digite seu ano de nascimento: '))
-    if date.today().year - idade < 18:
+for i in range(1, pessoas+1): # criar um range que inicia em 1 e vai até a quantidade de pessoas informado anteriormente, que também pode ser escrito com 6+1
+    idade = int(input('que ano a {} pessoa nasceu?: '.format(i))) #adicionado o contador "i" para aparecer
+    if date.today().year - idade < 18: # pegando o ano atual e o ano de nascimento e confere se é menor que 18 anos
         menor += 1 # será feita uma contagem na lista de pessoas menores de idade
     else:
         maior += 1 # será feita uma contagem na lista de pessoas maiores de idade
