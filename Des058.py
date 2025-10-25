a581e6b2c88fcec3e920d511d65df27a0b6d86d1
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2025-10-24 22:08:15 -0300

    adivinhar o computador
    melhorado o desafio 26, agora com para tentar adivinhar o numero que o computador escolheu, entre zero e dez, também foi adicionado um contador, para contabilizar com quantas tentativas acertou o resultado

---
+++ Des058.py
@@ -0,0 +1,16 @@
+'''melhor o jogo do desafio 026 onde o computador vai "pensar" em um numero entre 0 e 10.
+So que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites
+foram necessários para vencer'''
+
+from random import randint # biblioteca para escolher numeros aleatorios
+computador = randint(0, 10) # faixa entre 0 e 10
+cont = 1 # inicializando um contador em 1
+while True: # enquanto for verdadeira a condição a seguir
+    jogador = int(input('Digite um numero entre 0 e 10: ')) # informar o numero da tentativa
+    if jogador == computador: # quando o resultado for igual ao do computador finaliza
+        print('voce venceu!')
+        break
+    if jogador != computador: # enquanto for diferente vai contar mais um
+        cont += 1 # antes iniciado em 1, adiciona mais 1
+        continue
+print('Voce venceu com {} palpites.'.format(cont)) # no final vai escrever quantas tentativas foram para acertar
